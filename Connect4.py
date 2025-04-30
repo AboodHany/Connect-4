89a102a063a66727dba1ccfb5107416924f92e58
Ahmed Nasr <ahmed365j@gmail.com>
2025-04-30 17:41:21 +0300

    Update Connect4.py

--- Connect4.py
+++ Connect4.py
@@ -24,3 +24,42 @@ def get_next_open_row(board, col):
 
 def drop_piece(board, row, col, piece):
 	board[row][col] = piece
+def minimax(board, depth, maximizing_player):
+	valid_locations = get_valid_locations(board)
+	terminal = is_terminal_node(board)
+
+	if depth == 0 or terminal:
+		if terminal:
+			if winning_move(board, 2):
+				return (None, 10000000)
+			elif winning_move(board, 1):
+				return (None, -10000000)
+			else:
+				return (None, 0)
+		else:
+			return (None, 0)
+
+	if maximizing_player:
+		value = -math.inf
+		column = np.random.choice(valid_locations)
+		for col in valid_locations:
+			row = get_next_open_row(board, col)
+			b_copy = board.copy()
+			drop_piece(b_copy, row, col, 2)
+			new_score = minimax(b_copy, depth-1, False)[1] #recursion
+			if new_score > value:
+				value = new_score
+				column = col
+		return column, value
+	else:
+		value = math.inf
+		column = np.random.choice(valid_locations)
+		for col in valid_locations:
+			row = get_next_open_row(board, col)
+			b_copy = board.copy()
+			drop_piece(b_copy, row, col, 1)
+			new_score = minimax(b_copy, depth-1, True)[1]
+			if new_score < value:
+				value = new_score
+				column = col
+		return column, value
